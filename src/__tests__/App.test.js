3f4332546d1bf94913a2b2403d50cfb83eb3cb23
Michell Wambui Mbogo <mishie254@gmail.com>
2025-01-16 21:59:02 +0300

    react app d1

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,6 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { format } from "date-fns";
 import App from "../components/App";
-
 beforeEach(() => {
   render(<App />);
 });
